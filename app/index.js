351f13a1097be9e008c9c8eeec180a406bda6c2b
danny0533 <Danny@Danny-MacBook-Pro.local>
2017-09-13 12:33:03 +0800

    Commit

---
+++ app/index.js
@@ -0,0 +1,12 @@
+var express=require("express");
+var app=express();
+app.get("/",function(req,res){
+    res.send("Hello <b>World</b>");
+});
+app.listen(3000, function(){
+    console.log("Server started")
+});
+
+app.get("/test",function(req,res){
+    res.send("This is a page");
+});
