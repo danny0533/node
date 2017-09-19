82f7313f26f47a13417062a12b455d7a14f00023
danny0533 <Danny@Danny-MacBook-Pro.local>
2017-09-19 16:52:06 +0800

    commit

--- app/app.js
+++ app/app.js
@@ -1,8 +1,11 @@
 var http = require('http');
 var express=require("express");
 var app=express();
+var bodyParser = require('body-parser')
 
 app.use(express.static('.')) //ponit to CSS/JS folder
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/",function(req,res){
     res.sendFile(__dirname + '/index.html');
@@ -14,8 +17,12 @@ app.get("/home",function(req,res){
     console.log(__dirname);
 });
 
-app.get("/test",function(req,res){
-    res.send("This is a page");
+app.post("/sendform",function(req,res){
+    res.send(req.body.bike + "//" + req.body.country);
+});
+
+app.get("/ajax",function(req,res){
+    res.send("hello");
 });
 
 app.listen(3000, function(){
